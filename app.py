1d9384ecb810d0ad5597a458820d89fe371b3222
Saurabh Yadav <saurabh888191@gmail.com>
2022-07-09 16:03:54 +0530

    initial commit

--- app.py
+++ app.py
@@ -66,5 +66,3 @@ def about():
     return render_template("about.html")
 if __name__ == '__main__':
     app.run(host=None,port=None,debug=True)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
